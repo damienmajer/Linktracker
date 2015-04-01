82a7c29e3143c135b859293ef9fe716163861244
Rocker506 <rocker506@Roger-Mac.local>
2015-04-01 03:10:07 -0600

    EE 2.9.3 incompatibility fixed
    I solved a problem of incompatibility with EE 2.9.3. “Cp::
    set_variable()” is discontinued, change it for
    “$this->EE->view->cp_page_title”, now it works in versions greater than
    EE 2.6

--- system/expressionengine/third_party/linktracker/mcp.linktracker.php
+++ system/expressionengine/third_party/linktracker/mcp.linktracker.php
@@ -68,8 +68,9 @@ class Linktracker_mcp {
     /** -------------------------*/
     
     public function index()
-    {              
-        $this->EE->cp->set_variable('cp_page_title', $this->EE->lang->line('linktracker_module_name'));
+    {       
+
+        $this->EE->view->cp_page_title = $this->EE->lang->line('linktracker_module_name');
 
 		$vars = array(); $clicks = array(); $label = array();
 
@@ -105,7 +106,8 @@ class Linktracker_mcp {
 	
 	function reset() {
 
-		$this->EE->cp->set_variable('cp_page_title', $this->EE->lang->line('linktracker_reset'));
+		$this->EE->view->cp_page_title = $this->EE->lang->line('linktracker_reset');
+
 	 	$this->EE->cp->set_breadcrumb($this->base, $this->EE->lang->line('linktracker_module_name'));
 	 	
  		$id = $this->EE->input->get('link_id');
@@ -125,7 +127,8 @@ class Linktracker_mcp {
 	 	
 	 	$id = $this->EE->input->get('link_id');
 
-	 	$this->EE->cp->set_variable('cp_page_title', $this->EE->lang->line('linktracker_link_detail'));
+	 	$this->EE->view->cp_page_title = $this->EE->lang->line('linktracker_link_detail');
+	 	
 	 	$this->EE->cp->set_breadcrumb($this->base, $this->EE->lang->line('linktracker_module_name'));
 
     	$vars = array(); $clicks = array();
